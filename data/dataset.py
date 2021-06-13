b8cf1fde04bc9a20de646e1cfb4f3aa45d0bb087
johnnylord <johnnylord7410@gmail.com>
2021-06-13 12:00:43 +0800

    Speed up yolomaskdataset

--- data/dataset.py
+++ data/dataset.py
@@ -69,20 +69,15 @@ class YOLODataset:
         bboxes = np.loadtxt(fname=label_path, delimiter=" ", ndmin=2)
         bboxes = np.roll(bboxes, 4, axis=1).tolist() # (x, y, w, h, class)
         # Apply albumentation transformation
-        while True:
-            if self.transform is not None:
-                augmentations = self.transform(image=img, bboxes=bboxes)
-                tmp_img = augmentations['image']
-                tmp_bboxes = augmentations['bboxes']
-                if len(tmp_bboxes) != 0:
-                    img = tmp_img
-                    bboxes = tmp_bboxes
-                    break
-            else:
-                break
+        if self.transform is not None:
+            augmentations = self.transform(image=img, bboxes=bboxes)
+            img = augmentations['image']
+            bboxes = augmentations['bboxes']
         # Groundtruth cells placeholder
         targets = [ torch.zeros((self.n_anchors_per_scale, scale, scale, 6))
                     for scale in self.scales ]
+        if len(bboxes) == 0:
+            return img, targets
         # Create groundtruth across three scales
         # ====================================================================
         # - At least one cell will be assigned to an object
@@ -123,19 +118,18 @@ class YOLODataset:
                 ):
                     # For ignoring prediction (not count for loss)
                     targets[scale_idx][scale_anchor_idx, i, j, 4] = -1
-        return img, tuple(targets)
+        return img, targets
 
 
 class YOLOMaskDataset:
 
-    def __init__(self, csv_file, img_dir, mask_dir, label_dir,
+    def __init__(self, csv_file, img_dir, mask_dir,
                 anchors, scales=[13, 26, 52],
                 n_classes=80,
                 transform=None):
         self.annotations = pd.read_csv(csv_file)
         self.img_dir = img_dir
         self.mask_dir = mask_dir
-        self.label_dir = label_dir
         self.scales = scales
         self.anchors = torch.tensor(anchors[0]+anchors[1]+anchors[2]) # (3*3, 2)
         self.n_anchors_per_scale = self.anchors.size(0) // len(scales)
@@ -150,60 +144,64 @@ class YOLOMaskDataset:
         # Get metadata
         img_path = osp.join(self.img_dir, self.annotations.iloc[idx, 0])
         mask_dir = osp.join(self.mask_dir, self.annotations.iloc[idx, 0])
-        label_path = osp.join(self.label_dir, self.annotations.iloc[idx, 1])
 
         # Load img, instance masks, and instance bboxes
         img = np.array(Image.open(img_path).convert('RGB'))
-        bboxes = np.loadtxt(fname=label_path, delimiter=" ", ndmin=2)
-        bboxes = np.roll(bboxes, 4, axis=1).tolist() # (x, y, w, h, class)
 
-        # Load masks & bboxes
-        mask_files = [ osp.join(mask_dir, f) for f in os.listdir(mask_dir) ]
+        # Load masks
+        mask_files = sorted([
+                    osp.join(mask_dir, f)
+                    for f in os.listdir(mask_dir)
+                    if 'txt' not in f
+                    ])
         masks = []
         for mfile in mask_files:
             mask = np.uint8(Image.open(mfile))/255
             masks.append(mask)
 
+        # Load bboxes
+        bbox_files = sorted([
+                    osp.join(mask_dir, f)
+                    for f in os.listdir(mask_dir)
+                    if 'txt' in f
+                    ])
+        bboxes = []
+        for bfile in bbox_files:
+            bbox = np.loadtxt(fname=bfile, delimiter=" ").tolist()
+            bboxes.append(bbox)
+        bboxes = np.array(bboxes)
+        bboxes = np.roll(bboxes, 4, axis=1).tolist() # (x, y, w, h, class)
+        bboxes = [ box + [idx] for idx, box in enumerate(bboxes) ] # (x, y, w, h, class, idx)
+
         # Apply albumentation transformation
-        while True:
-            if self.transform is not None:
-                augmentations = self.transform(image=img, masks=masks, bboxes=bboxes)
-                tmp_img = augmentations['image']
-                tmp_masks = augmentations['masks']
-                tmp_bboxes = augmentations['bboxes']
-            if len(tmp_bboxes) != 0:
-                img = tmp_img
-                masks = tmp_masks
-                bboxes = tmp_bboxes
-                break
+        if self.transform is not None:
+            augmentations = self.transform(image=img, masks=masks, bboxes=bboxes)
+            img = augmentations['image']
+            masks = augmentations['masks']
+            bboxes = augmentations['bboxes']
 
         # Create ground truth targets
         # - 7 stands for (x_offset, y_offset, w_cell, h_cell, prob, class, mask)
         targets = [ torch.zeros((self.n_anchors_per_scale, scale, scale, 7))
                     for scale in self.scales ]
 
+        # Filter masks with valid bboxes
+        indices = np.array([ int(bbox[-1]) for bbox in bboxes ], dtype=np.uint8)
+        bboxes = np.array([ bbox[:5] for bbox in bboxes ])
+        if len(indices) > 0:
+            masks = np.array(masks)[indices]
+
+        if len(indices) == 0:
+            return img, torch.tensor(masks), targets
+
         # Compute iou between groundtruth bboxes & anchor boxes
         iou_anchors = iou_wh( # (N, 9)
                             torch.tensor(bboxes)[..., 2:4], # (N, 2)
                             self.anchors                    # (9, 2)
                             )
-        # Compute iou between groundtruth bboxes & segmentation masks
-        iou_masks = iou_bbox_mask( # (N, len(masks))
-                            torch.tensor(bboxes)[..., :4],  # (N, 4)
-                            torch.tensor(masks)             # (len(masks), h, w)
-                            )
-        box_indices, mask_indices = linear_sum_assignment(1-iou_masks)
-        boxId2maskId = dict([
-                        (bidx, midx)
-                        for bidx, midx in zip(box_indices, mask_indices) ])
-
         # Assign groundtruth object to the cell with the highest IoU value
         anchor_orders = iou_anchors.argsort(descending=True, dim=-1)
         for idx, anchor_order in enumerate(anchor_orders):
-            try:
-                mask_id = boxId2maskId[idx]
-            except Exception as e:
-                continue
             x, y, w, h, cls = bboxes[idx]
             # Check Flag
             has_obj_in_scale = [False]*len(self.scales)
@@ -223,7 +221,7 @@ class YOLOMaskDataset:
                     coord = torch.tensor([x_offset, y_offset, w_cell, h_cell])
                     targets[scale_idx][scale_anchor_idx, i, j, :4] = coord
                     targets[scale_idx][scale_anchor_idx, i, j, 5] = int(cls)
-                    targets[scale_idx][scale_anchor_idx, i, j, 6] = int(mask_id)
+                    targets[scale_idx][scale_anchor_idx, i, j, 6] = int(idx)
                     has_obj_in_scale[scale_idx] = True
                 elif (
                     not cell_has_obj
